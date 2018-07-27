3d5040c08cf5dc0e4e29026fca7520f98142bcee
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-27 22:49:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,6 +42,17 @@ class Song
     
   end
   
+  def self.artist_count
+    @@artists.each do |artist|
+      if @@artist_count[artist]
+        @@artist_count[artist] += 1
+      else
+        @@artist_count[artist] = 1
+      end
+    end
+      @@artist_count
+    
+  end
   
   
 end
